affa44284ca4dd82f3866cb6a78ec42f70170d47
xeijyx <colondrum@gmail.com>
2021-09-29 16:24:37 +0800

    fix

--- commands/queue.js
+++ commands/queue.js
@@ -1,19 +1,28 @@
 const Discord = require('discord.js')
 
 module.exports.run = (client, message, args, queue, searcher) => {
-    const serverQueue = queue.get(message.guild.id)
+    const serverQueue = queue.get(message.guild.id);
     if(!serverQueue)
         return message.channel.send("no music playing");
     if(message.member.voice.channel != message.guild.me.voice.channel)
         return message.channel.send("ur not in the vc")
-
-    let nowPlaying = serverQueue.songs[0];
-    let qMsg =  `playing: ${nowPlaying.title}\n------------------------\n`
-    for(var i = 1; i < serverQueue.songs; i++){
-        qMsg += `${i}, ${serverQueue.songs[i].title}\n`
-    }
-
     message.channel.send('```' + qMsg + 'requested by: ' + message.author.username + '```')
+}
+
+function embedGenerator(serverQueue){
+    const embeds = [];
+    let songs = 11;
+    for (let i = 1; i < serverQueue.songs.length; i += 10){
+        const current = serverQueue.songs.slice(i, songs)
+        songs += 10;
+        let j = i-1;
+        const info = current.map(song => `${++j}. [${song.title}](${song.url})`).join('\n')
+        const msg = new Discord.MessageEmbed()
+            .setDescription(`Now playing: [${serverQueue.songs[0].title}](${serverQueue.songs[0].url}) \n ${info}`)
+
+        embeds.push(msg)
+    }
+    return embeds;
 }
 
 module.exports.config = {
