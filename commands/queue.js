9204b50d8ac5da079daf6519494234c4e4427ff0
xeijyx <colondrum@gmail.com>
2021-09-29 16:30:00 +0800

    oiasdoiasjd

--- commands/queue.js
+++ commands/queue.js
@@ -1,12 +1,38 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
-module.exports.run = (client, message, args, queue, searcher) => {
+module.exports.run = async (client, message, args, queue, searcher) => {
     const serverQueue = queue.get(message.guild.id);
     if(!serverQueue)
-        return message.channel.send("no music playing");
+        return message.channel.send("There is no music currently playing!");
     if(message.member.voice.channel != message.guild.me.voice.channel)
-        return message.channel.send("ur not in the vc")
-    message.channel.send('```' + qMsg + 'requested by: ' + message.author.username + '```')
+        return message.channel.send("You are not in the voice channel!")
+
+    let currentPage = 0;
+    
+    const embeds = embedGenerator(serverQueue)
+
+    const queueEmbed = await message.channel.send(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage])
+    await queueEmbed.react('⬅️');
+    await queueEmbed.react('➡️');
+
+    const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && (message.author.id === user.id)
+    const collector = queueEmbed.createReactionCollector(reactionFilter);
+
+    collector.on('collect', (reaction, user) => {
+        if(reaction.emoji.name === '➡️'){
+            if(currentPage < embeds.length-1){
+                currentPage+=1;
+                queueEmbed.edit(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                message.reactions.resolve(reaction).users.remove(user)
+            }
+        }else if(reaction.emoji.name === '⬅️'){
+            if (currentPage !== 0){
+                currentPage -= 1;
+                queueEmbed.edit(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage])
+                message.reactions.resolve(reaction).users.remove(user)
+            }
+        }
+    })
 }
 
 function embedGenerator(serverQueue){
@@ -24,7 +50,6 @@ function embedGenerator(serverQueue){
     }
     return embeds;
 }
-
 module.exports.config = {
     name: "queue",
     description: "shows the queue",
